1d2ea4d2e7046996d2980360da259c22ee192668
bsuleymanov <contact@bsuleymanov.ml>
2021-04-20 17:26:05 +0500

    add tensor to image function.

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy.ndimage import grey_dilation, grey_erosion
 from pathlib import Path
 import torch
+import torchvision
+from PIL import Image
 
 def generate_trimap_from_alpha(masks):
     batch_size, height, width = masks.shape
@@ -25,3 +27,11 @@ def mkdir_if_empty_or_not_exist(dir_name):
 def denorm(tensor):
     out = (tensor + 1) / 2
     return out.clamp_(0, 1)
+
+
+def tensor_to_image(tensor):
+    grid = torchvision.utils.make_grid(tensor)
+    # Add 0.5 after unnormalizing to [0, 255] to round to nearest integer
+    ndarr = grid.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy()
+    im = Image.fromarray(ndarr)
+    return im
